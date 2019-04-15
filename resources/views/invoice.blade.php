f2a55c7579333fd38ca672271b8bb35117c029c9
LEXSI30 <justinedelabre30@gmail.com>
2019-04-15 23:22:30 +0200

    infraS

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -3,3 +3,38 @@
 INFRA SECURE</img>
 <h1 style="color: red">Bienvenu si vous êtes ici c'est pour crée votre projet</h1>
 <hr></hr>
+<center><textarea name="name" rows="8" cols="80">Les obligations a respecter pour bien monter votre dossier</textarea></center>
+
+<center><ul>
+<li>Mettez votre prenom</li>
+<li>Mettez votre nom famille</li>
+<li>Mettez votre numero telephone</li>
+<li>Mettez votre nom projet</li>
+<li>Mettez le type de projet que vous souhaitez</li>
+<h1>Si il s'agit developper un site web ou une application android ou apple ou de securisation reseaux, faire tester votre securiter reseaux par un professionnel en analyste en securiter ou par un pentester </h1>
+</ul></center>
+<hr></hr>
+<p>Nos partenariat</p>
+
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<img src="https://sysdream.com/assets/images/logo/sysdream.png" alt="">
